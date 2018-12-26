deebd9a68959172439148bdf1113a997743c0722
jaime <jgarciadiaz@groupon.com>
2018-12-26 16:41:22 -0600

    refactor: remove unused services

--- services/report.js
+++ services/report.js
@@ -6,35 +6,3 @@ export async function getReport(city = 'tijuana') {
   const response = await fetch(`${apiUrl}/report?city=${city}`)
   return await response.json()
 }
-
-export async function getUserReport(city = 'tijuana') {
-  const response = await fetch(`${apiUrl}/user/report?city=${city}`)
-  return await response.json()
-}
-
-export async function saveReport(data) {
-  const response = await fetch(`${apiUrl}/user/report`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  return await response.json()
-}
-
-export async function saveUserLocation(data) {
-  const response = await fetch(`${apiUrl}/user/location`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-  return await response.json()
-}
-
-export async function getUserLocation() {
-  const response = await fetch(`${apiUrl}/user/location`)
-  return await response.json()
-}
