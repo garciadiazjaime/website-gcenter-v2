4c44bd33550d8e8dd08e863fc54cdb0aeedd8f3f
jaime <jgarciadiaz@groupon.com>
2018-12-26 17:12:54 -0600

    feat: add 60 secs cache

--- services/report.js
+++ services/report.js
@@ -2,7 +2,26 @@ import 'isomorphic-unfetch'
 
 export const apiUrl = process && process.env && process.env.API_URL ? process.env.API_URL : window.apiUrl
 
+function hasExpired(lastValue) {
+  const currentValue = new Date().getTime()
+  const waitSecs = 60
+  return ((currentValue - lastValue) / 1000) > waitSecs
+}
+
 export async function getReport(city = 'tijuana') {
-  const response = await fetch(`${apiUrl}/report?city=${city}`)
-  return await response.json()
+  const cacheKey = `report:${city}`
+  let cache = JSON.parse(sessionStorage && sessionStorage.getItem(cacheKey))
+
+  if (!cache || !cache.report || hasExpired(cache.timestamp)) {
+    const response = await fetch(`${apiUrl}/report?city=${city}`)
+    const report = await response.json()
+    const data = {
+      report,
+      timestamp: new Date().getTime()
+    }
+    sessionStorage.setItem(cacheKey, JSON.stringify(data))
+    return report
+  }
+
+  return cache.report
 }
